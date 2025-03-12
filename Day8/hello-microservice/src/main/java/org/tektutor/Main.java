3a9e552f799db630fb64793e43602a74ac120c6d
Jeganathan Swaminathan <mail2jegan@gmail.com>
2025-03-12 12:48:06 +0530

    Added Hello microservice spring boot application in Day8

---
+++ Day8/hello-microservice/src/main/java/org/tektutor/Main.java
@@ -0,0 +1,16 @@
+package org.tektutor;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(Main.class, args);
+	}
+	
+}
